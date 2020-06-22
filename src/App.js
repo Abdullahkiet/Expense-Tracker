791e7c44eee900a1cf78e9fb3e0c5096fa56896f
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-22 22:38:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div>
 
-      Hello world here
-      
+      <Header></Header>
+
+
     </div>
   );
 }
