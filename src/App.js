b94aefe627c3ec195c9ef28220d2e76e49c35079
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-22 22:50:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Header></Header>
       <div className="container">
         <Balance></Balance>
+        <AccountSummary></AccountSummary>
+
       </div>
 
     </div>
