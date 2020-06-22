528ae125a2d2ba13498a1b23330e5439e061bc27
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-22 23:01:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
+
+//Components
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { AccountSummary } from './components/AccountSummary';
+import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
 
 //Provider
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div>
-
-      <Header></Header>
-      <div className="container">
-        <Balance></Balance>
-        <AccountSummary></AccountSummary>
-        <Transction></Transction>
-
-      </div>
-
-    </div>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <AccountSummary />
+          <TransactionHistory />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
   );
 }
 
