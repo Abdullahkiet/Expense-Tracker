9563fd0ff2259cdea6e059b048093e208295a89c
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-22 22:43:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 
 function App() {
   return (
     <div>
 
       <Header></Header>
-
+      <div className="container">
+        <Balance></Balance>
+      </div>
 
     </div>
   );
